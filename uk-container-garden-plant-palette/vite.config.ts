4609ecdd2ac8f1a119856cf52ba6cbfa4cd117d8
marcohey <62587460+marcohey@users.noreply.github.com>
2025-12-16 13:05:47 +0000

    Delete uk-container-garden-plant-palette directory

--- uk-container-garden-plant-palette/vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: './', // Ensures assets are loaded correctly on GitHub Pages (relative paths)
-});
